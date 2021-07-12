b707b602fadbcdd38cb29f4d57d22894e6f8f253
choiyeonseong <am3251@naver.com>
2021-07-12 15:29:46 +0900

    커밋

---
+++ test01.py
@@ -0,0 +1,17 @@
+n = 1
+name = 'choi'
+n = n + 2
+value = 1.2 * n
+
+print('{0} = 1.2 * {1}'.format(value,n))
+print(name)
+
+# 문자 인덱스 방법
+greeting='Hello World'
+# print(greeting[0])
+# print(greeting[5:8])
+print(greeting[:2])
+print(greeting[-2:])
+print(greeting*2)
+
+numbers=[0,1,2,3]
