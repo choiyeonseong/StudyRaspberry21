ebd267c2e8fea80767697644084b778003477ce7
choiyeonseong <am3251@naver.com>
2021-08-03 17:14:31 +0900

    0803 opencv 움직임 감지 테스트

--- opencv_ex01.py
+++ opencv_ex01.py
@@ -1,7 +1,7 @@
 import cv2
 
 # 기본 opencv 소스 - 이미지
-cam = cv2.VideoCapture(0)   #  기본 카메라
+cam = cv2.VideoCapture(0)   # 기본 카메라
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # 창 넓이
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # 창 높이
 
