b6df2b29358f694f514c2404544a64f25688566c
choiyeonseong <am3251@naver.com>
2021-08-04 16:27:35 +0900

    0804 움직임 감지

--- MoveDetecter.py
+++ MoveDetecter.py
@@ -1,18 +1,21 @@
 import cv2
-cap = cv2.VideoCapture(0)
 
-cap.set(3,300)
+cap = cv2.VideoCapture(0)   # 기본 카메라
+
+cap.set(3,300)  # 창 세팅
 cap.set(4,200)
-prev_frame = None
-ret, prev_frame=cap.read()  # 첫 영상 읽음
+
+ret, prev_frame = cap.read()  # 첫 영상 읽음
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read() # 웹캠 읽기
+   
     if ret: # 정상 읽기 일때만
-        f = cv2.absdiff(prev_frame, frame)
-        f = cv2.flip(f, 1)
+        f = cv2.absdiff(prev_frame, frame)  # 배경영상과 현재 프레임 영상의 차이를 구함
+        f = cv2.flip(f, 1)      # 좌우 반전 (1 : 좌우, 0 : 상하)
         cv2.imshow('img', f)    # 영상을 윈도우에 출력
         prev_frame = frame
+    
     k = cv2.waitKey(1)
     if k == 27: break   # ESC
 
